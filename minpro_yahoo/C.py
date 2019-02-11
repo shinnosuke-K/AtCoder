304e2840358dce6be8620e6a2b93ad4d20cd9d6b
shinnosuke-K <shin.050195@gmail.com>
2019-02-12 01:22:12 +0900

    not solve

--- minpro_yahoo/C.py
+++ minpro_yahoo/C.py
@@ -1,1 +1,12 @@
 # -*- coding: utf-8 -*-
+k, a, b = map(int, input().split())
+biscuits = 1
+money = 0
+for n in range(k):
+    if biscuits == a:
+        biscuits -= a
+        biscuits += b
+        n += 1
+    else:
+        biscuits += 1
+print(biscuits)
