0987d52a9b35d948ff0dba36bfb661075696ffa5
shinnosuke-K <shin.050195@gmail.com>
2019-10-19 22:11:07 +0900

    AC

--- ABC/141/B.py
+++ ABC/141/B.py
@@ -1,1 +1,14 @@
 # -*- coding: utf-8 -*-
+
+s = list(map(str, input()))
+for w in range(1, len(s), 2):
+    if 'R' in s[w]:
+        print("No")
+        exit()
+
+for w in range(0, len(s), 2):
+    if 'L' in s[w]:
+        print('No')
+        exit()
+
+print('Yes')
--- ABC/141/C.py
+++ ABC/141/C.py
@@ -1,1 +1,13 @@
 # -*- coding: utf-8 -*-
+n, k, q = map(int, input().split())
+c = [k - q for i in range(n)]
+a = [int(input()) for i in range(q)]
+for i in range(q):
+    c[a[i] - 1] += 1
+
+for i in range(n):
+    if c[i] >= 1:
+        print("Yes")
+    else:
+        print("No")
+
