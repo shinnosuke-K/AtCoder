3d96c499fb6a76e9eecde2563258da8c14dcb9f7
shinnosuke-K <shin.050195@gmail.com>
2019-10-19 22:15:40 +0900

    Not solve

--- ABC/138/F.py
+++ ABC/138/F.py
@@ -1,1 +1,11 @@
 # -*- coding: utf-8 -*-
+l, r = map(int, input().split())
+count = 0
+
+for x in range(l, r + 1):
+    for y in range(l, r + 1):
+        if (x % y) == (x ^ y):
+            count += 1
+            print(count)
+
+print(count % (10**9 + 7))
