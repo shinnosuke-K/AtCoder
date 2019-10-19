ab28eb4c91866efd373b994570c1fc0aeb5d3445
shinnosuke-K <shin.050195@gmail.com>
2019-10-19 22:14:36 +0900

    AC

--- ABC/138/A.py
+++ ABC/138/A.py
@@ -1,1 +1,5 @@
 # -*- coding: utf-8 -*-
+if int(input()) < 3200:
+    print('red')
+else:
+    print(input())
--- ABC/138/B.py
+++ ABC/138/B.py
@@ -1,1 +1,4 @@
 # -*- coding: utf-8 -*-
+n = int(input())
+b = [1 / i for i in list(map(int, input().split()))]
+print(1 / sum(b))
